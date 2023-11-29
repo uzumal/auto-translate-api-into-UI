2c10bc61bb09ed8869807abbf295d3ffb3c8899d
Kazuma Takeuchi <kazuma.takeuchi.1027@gmail.com>
2023-11-29 02:05:46 +0000

    update

--- components/OpenAIQueryComponent.tsx
+++ components/OpenAIQueryComponent.tsx
@@ -1,44 +1,57 @@
-import React, { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import Box from '@mui/material/Box';
-import JsxParser from 'react-jsx-parser';
-
-const ReflectCode = ({ code }) => {
-  // 安全なコード解析と実行
-  return <JsxParser jsx={code} />;
-};
 
 const OpenAIQueryComponent = ({ prompt }) => {
-  const [response, setResponse] = useState('');
+  const [code, setCode] = useState('');
+  const iframeRef = useRef(null);
 
   useEffect(() => {
-    if (prompt) {
-      fetch('https://api.openai.com/v1/engines/text-davinci-003/completions', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${process.env.NEXT_PUBLIC_OPENAI_API_KEY}`,
-        },
-        body: JSON.stringify({
-          prompt: prompt,
-          max_tokens: 150,
-        }),
-      })      
-      .then((res) => res.json())
-      .then((data) => {
-        if (data.choices && data.choices.length > 0) {
-          const code = data.choices[0].text.trim();
-          setResponse(code);
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://api.openai.com/v1/engines/text-davinci-003/completions', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${process.env.NEXT_PUBLIC_OPENAI_API_KEY}`,
+          },
+          body: JSON.stringify({
+            prompt: prompt,
+            max_tokens: 300,
+          }),
+        });
+
+        const data = await response.json();
+        console.log(data)
+        // 'text' プロパティから特定のパターンを切り抜く
+        if (data.choices && data.choices.length > 0 && data.choices[0].text) {
+          const fullText = data.choices[0].text.trim();
+          const importIndex = fullText.indexOf("import");
+          const codeSnippet = importIndex >= 0 ? fullText.substring(importIndex) : 'No code found.';
+          setCode(codeSnippet);
+          console.log(codeSnippet)
         } else {
-          console.error('No choices returned from OpenAI');
+          setCode('No code found.');
         }
-      })
-      .catch((err) => console.error('Error querying OpenAI:', err));
-    }
+      } catch (error) {
+        console.error('Error fetching data:', error);
+        setCode('Error loading code.');
+      }
+    };
+
+    fetchData();
   }, [prompt]);
 
+  useEffect(() => {
+    if (iframeRef.current && code) {
+      const iframe = iframeRef.current;
+      const doc = iframe.contentDocument || iframe.contentWindow.document;
+      doc.body.innerHTML = `<div style="white-space: pre-wrap;">${code}</div>`;
+    }
+  }, [code]);
+
   return (
-    <Box>
-      <ReflectCode code={response} />
+    <Box sx={{ width: '100%', height: '400px', border: '1px solid #ccc', overflow: 'auto' }}>
+      <iframe ref={iframeRef} style={{ width: '100%', height: '100%', border: 'none' }} />
     </Box>
   );
 };
